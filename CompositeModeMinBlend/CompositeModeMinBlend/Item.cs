ce252da3be3b23808a45fdddca2d3f1e9d447998
Tarkan Arslan <a@tarki.de>
2016-10-21 15:23:29 +0200

    CompositeModeMinBlend

---
+++ CompositeModeMinBlend/CompositeModeMinBlend/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace CompositeModeMinBlend
+{
+	public class Item
+	{
+		public string Text { get; set; }
+
+		public string Hint { get; set; }
+	}
+}
---
+++ CompositeModeMinBlend/CompositeModeMinBlend/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+
+namespace CompositeModeMinBlend
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+    {
+		public ObservableCollection<Item> Data { get; set; } = new ObservableCollection<Item>();
+
+		public MainPage()
+        {
+			for (int i = 0; i < 500000; i++)
+			{
+				Data.Add(new Item { Text = "Text " + i, Hint = "Hint " + i });
+			}
+
+            this.InitializeComponent();
+        }
+    }
+}
